be79d89cf86aa84321f8e01a8db845e6d7e3ba70
LucieCBurgess <lucie.burgess@icloud.com>
2016-10-28 19:06:48 +0100

    First commit to new repository. Added the file L4_logistic_regression_Default_dataset_27.10.2016 - exercise for lecture 4, using the Default dataset of credit card data. Calculated the linear and logistic regression and plotted basic charts. Drew the linear and logistic regression lines as straight lines.

---
+++ L4_Logistic_Regression_Default_dataset_27.10.2016.R
@@ -0,0 +1,52 @@
+rm(list=ls()) # removes all data
+
+# Load the libraries (NB. may need to use install.packages("ISLR"))
+library(ISLR)
+library(MASS)
+
+#View the Default dataset and fix it to the frame
+View(Default)
+fix(Default)
+?Default
+
+# Draw a scatterplot of balance and income
+plot(Default$balance,Default$income)
+
+# You can use colour to show a third dimension in the data, default = "No", the colour blue,for default = "Yes", the colour orange
+# And you can make this even more obvious by showing the shape of the data points
+plot(Default$balance,Default$income,col=ifelse(Default$default=='No',"blue","orange"),pch=ifelse(Default$default=='No',"o","x"))
+plot(Default$default,Default$balance,xlab="Default",ylab="Balance")
+
+# add colours by predefining a vector which is a sequence of colours
+colours=c("blue","orange")
+plot(Default$default,Default$balance,xlab="Default",ylab="Balance",col=colours)
+plot(Default$balance,Default$default,col="green")
+
+# Note the values of default are 1.0 (No) and 2.0 (Yes)
+# We really want these to be 0 and 1. The function as.numeric() turns a qualitative variable to a quantitative variable
+default_number<- as.numeric(Default$default)-1
+default_number
+plot(Default$balance,default_number,col="green")
+
+#Next plot the linear regression line
+lm(default_number~Default$balance,data=Default)
+linear_fit<-lm(default_number~Default$balance,data=Default)
+abline(linear_fit,col="red")
+
+# Next build the logistic regression model:
+glm(default_number~Default$balance,data=Default,family=binomial) 
+
+# NB. Using default_number or Default$default gives the same number in the logistic regression
+# Assign the same logistic_fit to the 
+logistic_fit<-glm(default_number~Default$balance,data=Default,family=binomial) 
+abline(logistic_fit,col="blue")
+summary(logistic_fit)
+
+#There are 3 ways to plot the logistic regression
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Data_analytics_R_exercises
+# This repository contains the R code for the Birkbeck course in data analytics
+# 2nd year MSc. computer science, 2016-17
